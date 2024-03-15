b7167a9ba219bd5bead5f2c4613a31029d0bb3ce
mirsaalf <mirsaalf@gmail.com>
2024-03-14 18:50:59 -0700

    Made b ook_id the key and autogenerated the value

--- Readdit/Models/Book.cs
+++ Readdit/Models/Book.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Readdit.Models
 {
     public class Book
     {
        
-        [Key] public int book_id { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int book_id { get; set; }
         public string book_name { get; set; }
         public string author_name { get; set; }
         public string book_genre { get; set;}
