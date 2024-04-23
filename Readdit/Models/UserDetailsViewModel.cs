8c6486d2db62bda0f37ac1e35f2c221c00fd8541
mirsaalf <mirsaalf@gmail.com>
2024-04-22 20:51:24 -0700

    Created a User Details Model

---
+++ Readdit/Models/UserDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Readdit.Areas.Identity.Data;
+
+namespace Readdit.Models
+{
+    public class UserDetailsViewModel
+    {
+            public ApplicationUser User { get; set; }
+            public IList<UserBook> UserBooks { get; set; }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public DateTime UserCreatedDate { get; set; }
+        
+    }
+}
