cce19657deef95749b1e25d682f51d1daec82cdb
mirsaalf <mirsaalf@gmail.com>
2024-06-09 21:47:17 -0700

    Made UserBooks navigation property nullable

--- Readdit/Models/Book.cs
+++ Readdit/Models/Book.cs
@@ -13,16 +13,27 @@ namespace Readdit.Models
 
     public class Book
     {
-       
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int book_id { get; set; }
+
+        [Required(ErrorMessage = "The Book Name field is required.")]
+        [StringLength(100)]
         public string book_name { get; set; }
+
+        [Required(ErrorMessage = "The Author Name field is required.")]
+        [StringLength(100)]
         public string author_name { get; set; }
-        public string book_genre { get; set;}
-        public DateTime date_added { get; set; }
+
+        [Required(ErrorMessage = "The Book Genre field is required.")]
+        [StringLength(50)]
+        public string book_genre { get; set; }
+
+        public DateTime date_added { get; set; } = DateTime.Now;
 
         // Navigation property for the UserBooks relationship
-        public ICollection<UserBook> UserBooks { get; set; }
+        [NotMapped]
+        public ICollection<UserBook>? UserBooks { get; set; }
     }
+
 }
